51220afce038ace49c574c45e3552e0bd71d2c53
Baisexodevs <140602823+Baisexodevs@users.noreply.github.com>
2024-01-11 16:28:51 +0500

    feat: product crud and search APIs completed

--- dist/controllers/Product.js
+++ dist/controllers/Product.js
@@ -29,3 +29,117 @@ export const newProduct = TryCatch(async (req, res, next) => {
         .status(201)
         .json({ success: true, message: "Product Created Successfully" });
 });
+export const getAllProducts = TryCatch(async (req, res, next) => {
+    const { search, sort, category, price } = req.query;
+    const page = Number(req.query.page) || 1;
+    const limit = Number(process.env.PRODUCT_PER_PAGE) || 8;
+    // This is how the skip knowledge works
+    // 1, 2, 3, 4, 5, 6, 7, 8
+    // 9, 10, 11, 12, 13, 14, 15, 16
+    // 17, 18, 19, 20, 21, 22, 23, 24
+    const skip = (page - 1) * limit;
+    const baseQuery = {
+    // price: {
+    //   $lte: Number(price),
+    // },
+    // category,
+    };
+    if (search)
+        baseQuery.name = {
+            $regex: search || "",
+            $options: "i",
+        };
+    if (price)
+        baseQuery.price = {
+            $lte: Number(price),
+        };
+    if (category)
+        baseQuery.category = category;
+    const productsPromise = Product.find(baseQuery)
+        .sort(sort && { price: sort === "asc" ? 1 : -1 })
+        .limit(limit)
+        .skip(skip);
+    const [products, filteredOnlyProduct] = await Promise.all([
+        productsPromise,
+        Product.find(baseQuery),
+    ]);
+    const totalPage = Math.ceil(filteredOnlyProduct.length / limit);
+    return res.status(200).json({
+        success: true,
+        products,
+        totalPage,
+    });
+});
+export const getLatestProducts = TryCatch(async (req, res, next) => {
+    const products = await Product.find().sort({ createdAt: -1 }).limit(5);
+    return res.status(200).json({
+        success: true,
+        products,
+    });
+});
+export const getAllCategories = TryCatch(async (req, res, next) => {
+    const categories = await Product.distinct("category");
+    return res.status(200).json({
+        success: true,
+        categories,
+    });
+});
+export const getAdminProducts = TryCatch(async (req, res, next) => {
+    const products = await Product.find({});
+    return res.status(200).json({
+        success: true,
+        products,
+    });
+});
+export const getSingleProduct = TryCatch(async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+    return res.status(200).json({
+        success: true,
+        product,
+    });
+});
+export const updateProduct = TryCatch(async (req, res, next) => {
+    const { id } = req.params;
+    const { name, price, stock, category } = req.body;
+    const photo = req.file;
+    const product = await Product.findById(id);
+    if (!product) {
+        return next(new ErrorHandler("Invalid Product Id Or Product Not Found", 404));
+    }
+    if (photo) {
+        rm(product.photo, () => {
+            console.log("Old Photo Removed");
+        });
+        product.photo = photo.path;
+    }
+    if (name)
+        product.name = name;
+    if (price)
+        product.price = price;
+    if (stock)
+        product.stock = stock;
+    if (category)
+        product.category = category.toLowerCase();
+    await product.save();
+    await res.status(200).json({
+        success: true,
+        message: "Product Updated Successfully",
+    });
+    return res
+        .status(201)
+        .json({ success: true, message: "Product Updated Successfully" });
+});
+export const deleteProduct = TryCatch(async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+        return next(new ErrorHandler("Invalid Product Id Or Product Not Found", 404));
+    }
+    rm(product.photo, () => {
+        console.log("Product Photo Deleted");
+    });
+    await Product.deleteOne();
+    return res.status(200).json({
+        success: true,
+        message: "Product Deleted Successfully",
+    });
+});
--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,13 +1,39 @@
 import express from "express";
-import { newProduct } from "../controllers/Product.js";
+import {
+  deleteProduct,
+  getAdminProducts,
+  getAllCategories,
+  getAllProducts,
+  getLatestProducts,
+  getSingleProduct,
+  newProduct,
+  updateProduct,
+} from "../controllers/Product.js";
 import { singleUpload } from "../middlewares/multer.js";
 import { adminOnly } from "../middlewares/auth.js";
 
 const app = express.Router();
 
-// route - /api/v1/user/new
+//  Create New Product - /api/v1/product/new
 app.post("/new", adminOnly, singleUpload, newProduct);
 
+// To get all Products with filters - /api/v1/product/all
+app.get('/all', getAllProducts)
 
+// To get last 5 Products - /api/v1/product/latest
+app.get("/latest", getLatestProducts);
+
+// To get all categories - /api/v1/product/category
+app.get("/category", getAllCategories);
+
+// To get all products - /api/v1/product/admin-products
+app.get("/admin-products", getAdminProducts);
+
+// To get, update, delete Product
+app
+  .route("/:id")
+  .get(getSingleProduct)
+  .put(singleUpload, updateProduct)
+  .delete(deleteProduct);
 
 export default app;
